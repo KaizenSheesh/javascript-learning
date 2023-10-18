6bfdd8e253f50a8640df7ceaf5ba107cc94f9d7e
KaizenSheesh <119792622+KaizenSheesh@users.noreply.github.com>
2023-10-18 16:51:50 +0700

    update:  loop js

---
+++ script.js
@@ -0,0 +1,27 @@
+// While Js
+// var jlmAngkot = 30;
+// var angkotBeroperasi = 20;
+// var nilaiAwal = 1;
+// while (nilaiAwal <= angkotBeroperasi) {
+//   console.log("Angkot No." + nilaiAwal + " Beroperasi dengan baik!");
+//   nilaiAwal++;
+// }
+
+// For Loop js
+// for (var nilaiAwal = angkotBeroperasi + 1;  nilaiAwal <= jlmAngkot; nilaiAwal++) {
+//     console.log('Angkot No. ' + nilaiAwal + ' Sedang diperbaiki!')
+// }
+
+// Pengkondisian
+var jlmAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+if (noAngkot <= angkotBeroperasi) {
+  for (noAngkot = angkotBeroperasi + 1; noAngkot <= jlmAngkot; noAngkot++) {
+    console.log('Angkot No')
+  }
+} else {
+  for (noAngkot = angkotBeroperasi + 1; noAngkot <= jlmAngkot; noAngkot++) {
+    console.log("Angkot No. " + noAngkot + " sedang diperbaiki!");
+  }
+}
